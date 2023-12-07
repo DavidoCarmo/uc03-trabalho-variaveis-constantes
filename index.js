1c31ca047f6bc8f1c34f8353324e07315d66d605
davi do carmo <davigcarmo02@gmail.com>
2023-12-07 20:20:27 -0300

    entrada de dados

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-''
+const readline = require("readline-sync");
+
+// calculo de media de notas de um bimestre 
+
+// entrada
+// o usuario deve informar 4 notas
+let nota_prova_1 = readline.questionFloat("Qual a nota da prova 1?\n");
+let nota_prova_2 = readline.questionFloat("Qual a nota da prova 2?\n");
+let nota_prova_3 = readline.questionFloat("Qual a nota da prova 3?\n");
+let nota_prova_4 = readline.questionFloat("Qual a nota da prova 4?\n");
+
+console.log(nota_prova_1, nota_prova_2, nota_prova_3, nota_prova_4);
+
+// processamento 
+
+//saida
