993f8fd74d6f0128e48787b285b80b0d2d2ee9cb
davi do carmo <davigcarmo02@gmail.com>
2023-12-07 21:39:46 -0300

    finalizado a saida de dados

--- index.js
+++ index.js
@@ -4,13 +4,32 @@ const readline = require("readline-sync");
 
 // entrada
 // o usuario deve informar 4 notas
-let nota_prova_1 = readline.questionFloat("Qual a nota da prova 1?\n");
-let nota_prova_2 = readline.questionFloat("Qual a nota da prova 2?\n");
-let nota_prova_3 = readline.questionFloat("Qual a nota da prova 3?\n");
-let nota_prova_4 = readline.questionFloat("Qual a nota da prova 4?\n");
+const nota_prova_1 = readline.questionFloat("Qual a nota da prova 1?\n");
+const nota_prova_2 = readline.questionFloat("Qual a nota da prova 2?\n");
+const nota_prova_3 = readline.questionFloat("Qual a nota da prova 3?\n");
+const nota_prova_4 = readline.questionFloat("Qual a nota da prova 4?\n");
 
 console.log(nota_prova_1, nota_prova_2, nota_prova_3, nota_prova_4);
 
-// processamento 
+// PROCESSAMENTO
 
-//saida
+//somar todas as notas e armazenar o valor em uma variavel
+
+//operadores aritimeticos
+//soma(+)
+//subtração(-)
+//divisao (/)
+//multiplicaçao(*)
+
+const soma_notas = nota_prova_1 + nota_prova_2 + nota_prova_3 + nota_prova_4;
+
+console.log(soma_notas);
+
+//calcular a medida e calcular em uma variavel
+
+const media_notas = soma_notas / 4;
+
+//SAIDA
+//interpolação de texto
+
+console.log(`A media das notas é: ${media_notas}`);
